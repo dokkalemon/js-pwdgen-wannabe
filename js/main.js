fbf0b24cd986c32b063efdc72ff2ddd257036704
dokkalemon <dottori.roberto@gmail..com>
2021-10-20 15:42:07 +0200

    create the password generator function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 /* Chiediamo il nome all'utente */
+const name = prompt('Quale è il tuo nome?');
+
 
 /* Chiediamo il cognome all'utente */
+const surname = prompt('Quale è il tuo cognome?');
 
 /* Chiediamo il suo colore preferito */
+const color = prompt('Quale è il tuo colore preferito?');
+
 
 /* Scrivere i dati ricevuti sulla pagina */
+// Scrivere i dati con l'operatore di concatenazione
+/* document.getElementById('result').innerHTML = name + surname + color + '21' */
+
+// Scrivere i dati con i Template Literal
+document.getElementById('result').innerHTML = `${name}${surname}${color}21`
